60b9fe883bc280543bd03cc26c64c048b080c6a6
serek90 <43785920+serek90@users.noreply.github.com>
2020-09-16 12:07:26 +0200

    Delete .hello.ko.cmd

--- HelloKernelModule/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/serek/programs/githubC/Linux/Linux/HelloModule/hello.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/serek/programs/githubC/Linux/Linux/HelloModule/hello.ko /home/serek/programs/githubC/Linux/Linux/HelloModule/hello.o /home/serek/programs/githubC/Linux/Linux/HelloModule/hello.mod.o ;  true
