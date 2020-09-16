67b9131259aa5195fc7727bc18f7da28b593d6c1
serek90 <43785920+serek90@users.noreply.github.com>
2020-09-16 12:07:36 +0200

    Delete .hello.mod.cmd

--- HelloKernelModule/.hello.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/serek/programs/githubC/Linux/Linux/HelloModule/hello.mod := { echo  /home/serek/programs/githubC/Linux/Linux/HelloModule/hello.o; echo; } > /home/serek/programs/githubC/Linux/Linux/HelloModule/hello.mod
