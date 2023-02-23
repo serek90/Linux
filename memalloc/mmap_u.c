3142794e918c463d301d7dea6e18a07942b3b59a
Jakub Seroczynski <43785920+serek90@users.noreply.github.com>
2023-02-23 19:12:59 +0100

    Add deallocation mem

--- memalloc/mmap_u.c
+++ memalloc/mmap_u.c
@@ -8,8 +8,6 @@
 int main()
 {
         void *pbrk = sbrk(0);
-	
-	/* First allocation */
         printf("Addres before mmap: %p\n", pbrk);
         void *first = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
                                 MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
@@ -20,7 +18,6 @@ int main()
         pbrk = sbrk(0);
         printf("Addres after first mmap: %p\n", pbrk);
 
-	/* Second allocation */
         void *second = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
                                 MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
         if(second == MAP_FAILED)
@@ -31,8 +28,38 @@ int main()
         printf("Addres after all mmap: %p\n", pbrk);
 
 
+
         printf("First mmap:  %p\n", first);
         printf("Second mmap: %p\n", second);
+        printf("\n\n");
+
+
+        /* Deallocate mem */
+        printf("Deallocating\n");
+        if(munmap(second, PAGE_SIZE))
+        {
+                printf("Unmapping failed\n");
+        }
+        if(second)
+        {
+                printf("Mem is not set to NULL\n");
+        }
+
+        pbrk = sbrk(0);
+        printf("Addres after unmapping: %p\n", pbrk);
+
+        if(munmap(first, PAGE_SIZE))
+        {
+                printf("Unmapping failed\n");
+        }
+        if(first)
+        {
+                printf("Mem is not set to NULL\n");
+        }
+
+        pbrk = sbrk(0);
+        printf("Addres after unmapping: %p\n", pbrk);
+
 
         return 0;
 }
