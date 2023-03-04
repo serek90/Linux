0f728edb75954a7510ab021d9c1c8a853c22f3a6
Jakub Seroczynski <43785920+serek90@users.noreply.github.com>
2023-03-04 09:38:04 +0100

    Reduce length of message

--- SystemPrograming/IPC/msqQ_client.c
+++ SystemPrograming/IPC/msqQ_client.c
@@ -4,36 +4,37 @@
 #include <errno.h>
 #include <string.h>
 
-#define MSG_BUFFER_SIZE 100
+#define MSG_BUFFER_SIZE 30
 #define PMODE 0666
 int main( int argc, char ** argv)
 {
         char buffer[MSG_BUFFER_SIZE];
         int msgQ_fd;
 
-        strcpy(buffer, "First message");
+        strcpy(buffer, "First");
 
         struct mq_attr attr;
         attr.mq_maxmsg = 20;
         attr.mq_msgsize = 128;
         attr.mq_flags   = 0;
         /* open message queue */
-        msgQ_fd = mq_open("/myipc", O_WRONLY | O_CREAT, PMODE, &attr);
+        msgQ_fd = mq_open("/my_queue", O_WRONLY, 0, 0);
         if(msgQ_fd == -1)
         {
                 perror("client: mq_open failed");
                 exit(1);
         }
 
-        /* send message */
-        if(mq_send(msgQ_fd, buffer, MSG_BUFFER_SIZE /* strlen ?? */, 0) == -1)
+        /* sedn message */
+        if(mq_send(msgQ_fd, buffer, /* MSG_BUFFER_SIZE */ strlen(buffer) + 1, 0) == -1)
         {
-                perror("client: Not able to send message to server\n");
+                perror("client: Not able to send message to server");
                 exit(1);
         }
-        printf("Message was sent\n");
+        printf("Message was sent: %s - %d\n", buffer, strlen(buffer));
 
         mq_close(msgQ_fd);
 
         return 0;
 }
+
